a26321a39dabe4f84e7c3ad578dcb630570df078
marpet <marco.peters@brockmann-consult.de>
2010-11-22 10:56:07 +0100

    internal changes

--- coastcolour-fuzzy-classification/src/main/java/org/esa/beam/coastcolour/fuzzy/FuzzyClassification.java
+++ coastcolour-fuzzy-classification/src/main/java/org/esa/beam/coastcolour/fuzzy/FuzzyClassification.java
@@ -28,22 +28,16 @@ public class FuzzyClassification {
             final Matrix yInvMatrix = new Matrix(yInvers);
             final Matrix matrixB = yInvMatrix.times(new Matrix(y, y.length));  // b
             double zSquare = 0;
-            final double[] b = new double[bandCount];
-            b[0] = matrixB.getArray()[0][0];
-            b[1] = matrixB.getArray()[1][0];
-            b[2] = matrixB.getArray()[2][0];
-            b[3] = matrixB.getArray()[3][0];
-            b[4] = matrixB.getArray()[4][0];
             for (int j = 0; j < bandCount; j++) {
-                zSquare += y[j] * b[j];
+                zSquare += y[j] * matrixB.getArray()[j][0];
             }
             double x = zSquare / 2.0;   // no idea why this is needed. Even Tim doesn't have
-            double chiSquare = bandCount / 2.0;  // chiz
+            double chiSquare = bandCount / 2.0;
             if (x <= (chiSquare + 1.0)) {
-                double gamma = getIncompleteGammaFunction(chiSquare, x); // gser
+                double gamma = computeIGFSeries(chiSquare, x);
                 alphaChi[i] = 1.0 - gamma;
             } else {
-                double gamma = gcf(chiSquare, x);
+                double gamma = computeIGFContinuedFraction(chiSquare, x);
                 alphaChi[i] = gamma;
             }
         }
@@ -52,81 +46,75 @@ public class FuzzyClassification {
 
     }
 
-    private static double gcf(double a, double x) {
-        final double FPMIN = 1.0e-30;
-        final int maxIteration = 100;
-        final double eps = 3.0e-7;
-
-        double gln = gammln(a);
+    // Computes the incomplete gamma function by its continued fraction
+    private static double computeIGFContinuedFraction(double a, double x) {
+        final double min = 1.0e-30;
+        final double constFactor = Math.exp(-x + a * Math.log(x) - logGamma(a));
         double b = x + 1.0 - a;
-        double c = 1.0 / FPMIN;
+        double c = 1.0 / min;
         double d = 1.0 / b;
         double h = d;
-        for (int i = 1; i <= maxIteration; i++) {
+        for (int i = 1; i <= 100; i++) {
             double an = -i * (i - a);
             b += 2.0;
             d = an * d + b;
-            if (Math.abs(d) < FPMIN) {
-                d = FPMIN;
-            }
             c = b + an / c;
-            if (Math.abs(c) < FPMIN) {
-                c = FPMIN;
+            if (Math.abs(d) < min) {
+                d = min;
+            }
+            if (Math.abs(c) < min) {
+                c = min;
             }
             d = 1.0 / d;
             double del = d * c;
             h *= del;
-            if (Math.abs(del - 1.0) < eps) {
-                break;
-            }
-            if (i > maxIteration) {
-                throw new IllegalArgumentException("a too large, ITMAX too small in gcf");
+            if (Math.abs(del - 1.0) < 3.0e-7) {
+                return constFactor * h;
             }
         }
-        return Math.exp(-x + a * Math.log(x) - (gln)) * h;
+        throw new IllegalArgumentException("Parameter 'a' is too large");
     }
 
-    private static double getIncompleteGammaFunction(double a, double x) {
-        if (x <= 0.0) {
-            if (x < 0.0) {
-                throw new IllegalArgumentException("x less than 0 in routine gser");
-            }
-            return 0.0;
-        } else {
-            double ap = a;
+    // Computes the incomplete gamma function by its series representation
+    private static double computeIGFSeries(double a, double x) {
+        if (x < 0.0) {
+            throw new IllegalArgumentException("x must be greater or equal to zero");
+        }
+        if (x > 0.0) {
+            double incA = a;
             double sum = 1.0 / a;
             double del = sum;
 
             final int maxIteration = 100;
             final double eps = 3.0e-7;
+            final double constFactor = Math.exp(-x + a * Math.log(x) - logGamma(a));
             for (int i = 1; i <= maxIteration; i++) {
-                ++ap;
-                del *= x / ap;
+                del *= x / ++incA;
                 sum += del;
                 if (Math.abs(del) < Math.abs(sum) * eps) {
-                    final double gammaLog = gammln(a);
-                    return sum * Math.exp(-x + a * Math.log(x) - (gammaLog));
+                    return sum * constFactor;
                 }
             }
-            throw new IllegalArgumentException("a too large, ITMAX too small in routine gser");
+            throw new IllegalArgumentException("Parameter 'a' is too large");
+        } else {
+            return 0.0;
         }
     }
 
-    private static double gammln(double a) {
-        final double[] cof = {
-                76.18009172947146, -86.50532032941677,
-                24.01409824083091, -1.231739572450155,
-                0.1208650973866179e-2, -0.5395239384953e-5
+    private static double logGamma(double x) {
+        final double[] coefficients = {
+                76.18009172947146, -86.50532032941677, 24.01409824083091,
+                -1.231739572450155, 0.1208650973866179e-2, -0.5395239384953e-5
         };
-        double y = a;
+        double tempX = x;
 
-        double tmp = a + 5.5;
-        tmp -= (a + 0.5) * Math.log(tmp);
-        double ser = 1.000000000190015;
-        for (int j = 0; j <= 5; j++) {
-            ser += cof[j] / ++y;
+        double tmp = x + 5.5;
+        tmp -= (x + 0.5) * Math.log(tmp);
+        double sum = 1.000000000190015;
+        for (int i = 0; i <= 5; i++) {
+            sum += coefficients[i] / ++tempX;
         }
-        return -tmp + Math.log(2.5066282746310005 * ser / a);
+        return -tmp + Math.log(2.5066282746310005 * sum / x);
     }
 
 }

d2d99d9c214970ee5820f2504ee452f17cf620c5
Olaf Danne <olaf.danne@brockmann-consult.de>
2014-06-13 17:53:15 +0200

    increased OWT version to preliminary provide standalone jar

--- beam-owt-classification/src/main/java/org/esa/beam/owt/OWT_TYPE.java
+++ beam-owt-classification/src/main/java/org/esa/beam/owt/OWT_TYPE.java
@@ -30,35 +30,36 @@ public enum OWT_TYPE {
             return wavelength;
         }
     },
-    COASTAL_6BAND {
-        private float[] wavelength = new float[]{410, 443, 490, 510, 555, 670};
-
-        @Override
-        AuxdataFactory getAuxdataFactory() {
-            return new CoastalAuxdataFactory("/auxdata/coastal/owt16_meris_stats_101119_6band.hdf");
-        }
-
-        @Override
-        int getClassCount() {
-            return 9;
-        }
-
-        @Override
-        double[] mapMembershipsToClasses(double[] memberships) {
-            double[] classes = new double[getClassCount()];
-            System.arraycopy(memberships, 0, classes, 0, 8);
-            // setting the value for the 9th class to the sum of the last 8 classes
-            for (int i = 8; i < memberships.length; i++) {
-                classes[8] += memberships[i];
-            }
-            return classes;
-        }
-
-        @Override
-        float[] getWavelengths() {
-            return wavelength;
-        }
-    },
+    // exclude this (CB, 20140613):
+//    COASTAL_6BAND {
+//        private float[] wavelength = new float[]{410, 443, 490, 510, 555, 670};
+//
+//        @Override
+//        AuxdataFactory getAuxdataFactory() {
+//            return new CoastalAuxdataFactory("/auxdata/coastal/owt16_meris_stats_101119_6band.hdf");
+//        }
+//
+//        @Override
+//        int getClassCount() {
+//            return 9;
+//        }
+//
+//        @Override
+//        double[] mapMembershipsToClasses(double[] memberships) {
+//            double[] classes = new double[getClassCount()];
+//            System.arraycopy(memberships, 0, classes, 0, 8);
+//            // setting the value for the 9th class to the sum of the last 8 classes
+//            for (int i = 8; i < memberships.length; i++) {
+//                classes[8] += memberships[i];
+//            }
+//            return classes;
+//        }
+//
+//        @Override
+//        float[] getWavelengths() {
+//            return wavelength;
+//        }
+//    },
     INLAND {
 
         private float[] wavelength = new float[]{412, 443, 490, 510, 560, 620, 665, 680, 709, 754};
@@ -109,31 +110,33 @@ public enum OWT_TYPE {
             return memberships;
         }
     },
-    INLAND_WITHOUT_443 {
-
-        private float[] wavelength = new float[]{412, 490, 510, 560, 620, 665, 680, 709, 754};
-
-        @Override
-        AuxdataFactory getAuxdataFactory() {
-            return new InlandAuxdataFactory(wavelength);
-        }
-
-        @Override
-        float[] getWavelengths() {
-            return wavelength;
-
-        }
-
-        @Override
-        int getClassCount() {
-            return 7;
-        }
-
-        @Override
-        double[] mapMembershipsToClasses(double[] memberships) {
-            return memberships;
-        }
-    };
+    // exclude this (CB, 20140613):
+//    INLAND_WITHOUT_443 {
+//
+//        private float[] wavelength = new float[]{412, 490, 510, 560, 620, 665, 680, 709, 754};
+//
+//        @Override
+//        AuxdataFactory getAuxdataFactory() {
+//            return new InlandAuxdataFactory(wavelength);
+//        }
+//
+//        @Override
+//        float[] getWavelengths() {
+//            return wavelength;
+//
+//        }
+//
+//        @Override
+//        int getClassCount() {
+//            return 7;
+//        }
+//
+//        @Override
+//        double[] mapMembershipsToClasses(double[] memberships) {
+//            return memberships;
+//        }
+//    }
+    ;
 
     abstract AuxdataFactory getAuxdataFactory();
 

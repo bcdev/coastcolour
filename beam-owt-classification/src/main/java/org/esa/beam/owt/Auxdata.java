a282bfecc2c2e5b2c0066e21f2adf81ccf9b6a0c
marpet <marco.peters@brockmann-consult.de>
2014-08-08 10:38:34 +0200

    removed deprecated constructor

--- beam-owt-classification/src/main/java/org/esa/beam/owt/Auxdata.java
+++ beam-owt-classification/src/main/java/org/esa/beam/owt/Auxdata.java
@@ -1,28 +1,9 @@
 package org.esa.beam.owt;
 
-import ucar.ma2.InvalidRangeException;
-
-import java.io.IOException;
-import java.net.URI;
-
 public class Auxdata {
 
     private double[][] spectralMeans;
     private double[][][] invCovarianceMatrices;
-
-    /**
-     * @deprecated Implement a {@link AuxdataFactory} instead.
-     */
-    @Deprecated
-    public Auxdata(URI filePath) throws IOException, InvalidRangeException {
-        try {
-            Auxdata auxdata = new CoastalAuxdataFactory("/auxdata/coastal/owt16_meris_stats_101119_5band.hdf").createAuxdata();
-            this.spectralMeans = auxdata.spectralMeans;
-            this.invCovarianceMatrices = auxdata.invCovarianceMatrices;
-        } catch (AuxdataFactory.Exception e) {
-            throw new IllegalStateException("Auxdata could not be initialised", e);
-        }
-    }
 
     public Auxdata(double[][] spectralMeans, double[][][] invCovarianceMatrices) {
         this.spectralMeans = spectralMeans;
